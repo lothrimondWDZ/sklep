f94b2c4ce21072c7eb27b3b4e8e2e6b138c67765
jakasnazwa <rcyniak@o2.pl>
2016-01-29 15:34:14 +0100

    część komentarzy

--- src/model/Tshirt.java
+++ src/model/Tshirt.java
@@ -5,16 +5,37 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * 
+ * Klasa reprezentuj¹ca T-shirt
+ *
+ */
 @XmlRootElement(name = "tshirt")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Tshirt extends Shirt {
-	@XmlElement(type = TshirtSize.class)
+    /**
+     * pole oznaczaj¹ce rozmiar T-shirt
+     */
+    @XmlElement(type = TshirtSize.class)
     private TshirtSize size;
 
+    /**
+     * Funkcja zwracaj¹ca rozmiar T-shirt
+     * 
+     * @return {@link TshirtSize} parametrem zwracanym rozmiar
+     */
+    @Override
     public TshirtSize getSize() {
 	return size;
     }
 
+    /**
+     * Funkcja ustawiaj¹ca rozmiar T-shirt
+     * 
+     * @param size
+     *            przyjmowany jest rozmiar w postaci {@link TshirtSize}
+     */
+    @Override
     public void setSize(TshirtSize size) {
 	this.size = size;
     }
